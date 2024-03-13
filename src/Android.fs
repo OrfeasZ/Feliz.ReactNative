b5a54f1989669b073112de9222774dbcb9cf5ab0
Voltide <michaeltugby0@gmail.com>
2024-03-13 23:02:30 +0000

    Add android module types

---
+++ src/Android.fs
@@ -0,0 +1,17 @@
+﻿namespace Feliz.ReactNative
+
+open Fable.Core
+open Fable.Core.JsInterop
+
+[<Erase>]
+type NativeEventSubscription =
+    abstract remove: unit -> unit
+
+[<Erase>]
+type BackHandler =
+    abstract addEventListener: string -> (unit -> bool) -> NativeEventSubscription
+    abstract exitApp: unit -> unit
+    abstract removeEventListener: string -> (unit -> bool) -> unit
+
+module Android =
+    let BackHandler: BackHandler = import "BackHandler" "react-native"
