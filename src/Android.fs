4aa580069d9b8cc669ce5ea797a7c086d6bd1917
Voltide <michaeltugby0@gmail.com>
2024-03-14 11:59:56 +0000

    Create a string enum for BackPressEventName

--- src/Android.fs
+++ src/Android.fs
@@ -3,15 +3,18 @@
 open Fable.Core
 open Fable.Core.JsInterop
 
+[<StringEnum(CaseRules.LowerFirst)>]
+type BackPressEventName = | HardwareBackPress
+
 [<Erase>]
 type NativeEventSubscription =
     abstract remove: unit -> unit
 
 [<Erase>]
 type BackHandler =
-    abstract addEventListener: string -> (unit -> bool) -> NativeEventSubscription
+    abstract addEventListener: BackPressEventName -> (unit -> bool) -> NativeEventSubscription
     abstract exitApp: unit -> unit
-    abstract removeEventListener: string -> (unit -> bool) -> unit
+    abstract removeEventListener: BackPressEventName -> (unit -> bool) -> unit
 
 module Android =
     let BackHandler: BackHandler = import "BackHandler" "react-native"
